5ff9242417d205e17de9b18660969b2342234f08
yadokani389 <yadokani389@gmail.com>
2025-12-29 13:50:19 +0900

    fix: remove unused access_token from CreateToken

--- item-manager/kernel/src/model/auth/event.rs
+++ item-manager/kernel/src/model/auth/event.rs
@@ -2,14 +2,10 @@ use crate::model::id::UserId;
 
 pub struct CreateToken {
     pub user_id: UserId,
-    pub access_token: String,
 }
+
 impl CreateToken {
     pub fn new(user_id: UserId) -> Self {
-        let access_token = uuid::Uuid::new_v4().simple().to_string();
-        Self {
-            user_id,
-            access_token,
-        }
+        Self { user_id }
     }
 }
